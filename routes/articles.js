22956bbd7d4eb14fb438969cb1a8be80681388ab
Ahindra D <official.ahindra@gmail.com>
2022-08-17 17:42:37 +0530

    upadate needs work

--- routes/articles.js
+++ routes/articles.js
@@ -8,6 +8,36 @@ router.get('/new', (request, response) => {
     response.render('articles/new');
 })
 
+
+router.get('/details/:id', async (request, response) => {
+    console.log(request.params.id);
+    let article = await Article.findById(request.params.id);
+    response.render('articles/details', { article: article });
+})
+
+router.get('/delete/:id', async (request, response) => {
+    console.log(request.params.id);
+    await Article.deleteOne({ _id: request.params.id });
+    response.redirect('/');
+})
+
+router.get('/edit/:id', async (request, response) => {
+    console.log(request.params.id);
+    let article = await Article.findById(request.params.id);
+    console.log(article);
+    response.render('articles/update', { article: article });
+})
+router.get('/update/:id', async (request, response) => {
+    console.log(request.params.id);
+    console.log(request.body);
+    await Article.updateOne({ _id: request.params.id }, {
+        title: request.body.title,
+        body: request.body.desc,
+        author: request.body.author,
+    });
+    response.redirect('/');
+})
+
 router.post('/add', async (request, response) => {
     console.log(request.body);
     let article = new Article({
